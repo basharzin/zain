9ea39299e89d9f34318df72184bcbca16fe13267
bashar249625 <bashar249625@bue.edu.eg>
2025-10-22 04:44:35 -0700

    secedit

--- My project/Assets/bmen.cs
+++ My project/Assets/bmen.cs
@@ -18,10 +18,11 @@ public class bmen : MonoBehaviour
     public float groundCheckRadius; 
     public LayerMask whatIsGround; 
     private bool grounded; 
-
+    private Animator anim;
     
     void Start()
     {
+        anim = GetComponent<Animator>();
     }
 
     
@@ -66,6 +67,15 @@ public class bmen : MonoBehaviour
                 GetComponent<SpriteRenderer>().flipX = false;
             }
         }
+
+        anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+
+        anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
+
+
+        anim.SetBool("Grounded", grounded);
+
     }
 
 
