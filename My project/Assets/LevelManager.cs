c02e0d02e539de853898d8340cf9e2dbb1ba1a6c
bashar249625 <bashar249625@bue.edu.eg>
2025-11-04 11:11:42 +0200

    4ed

---
+++ My project/Assets/LevelManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public GameObject CurrentCheckpoint; 
+   
+
+    public Transform player;
+   
+    void Start() 
+    {
+      
+        CurrentCheckpoint = null;
+    }
+
+    void Update() 
+    {
+ 
+         
+    }
+    
+
+         
+    public void RespawnPlayer()
+    {
+      
+        FindObjectOfType<bmen>().transform.position = CurrentCheckpoint.transform.position;
+
+       
+    }
+}   
